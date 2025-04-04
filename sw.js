1f0b1a9188d9c8d7c6607ac5f47ef31da5639dbb
CSECRM59 <csecrm59300@gmail.com>
2025-04-04 19:34:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('cse-cache-v1').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js'
+        './', // Chemin relatif pour la racine
+        './index.html',
+        './styles.css',
+        './script.js'
       ]);
     })
   );
